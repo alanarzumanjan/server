ae65e8a38bd58c78db983b2ceb318a0a52cbe3a7
Alans Arzumanjans <117813243+alanarzumanjan@users.noreply.github.com>
2024-03-08 09:00:42 +0000

    Sleep

--- myclient.cpp
+++ myclient.cpp
@@ -2,49 +2,80 @@
 #include <netinet/in.h>
 #include <cstring>
 #include <unistd.h>
-#include <cstring>
 #include <sys/socket.h>
 #include <arpa/inet.h>
+#include <string>
+#include <thread>
 
-int PORT = 8888;
 using namespace std;
 
-int main(int argc, char *argv[]) {
+int PORT = 8888;
+const int BUFFER_SIZE = 1024;
 
+void read_messages(int sock) {
+    while (true) {
+        char buffer[BUFFER_SIZE] = {0};
+        long valread = read(sock, buffer, BUFFER_SIZE);
+        if (valread == 0) {
+            cout << "Server disconnected" << endl;
+            close(sock);
+            exit(0);
+        } else if (valread < 0) {
+            cout << "Read error" << endl;
+            close(sock);
+            exit(1);
+        }
+        cout << "Message Received: " << buffer << endl;
+    }
+}
+
+void write_messages(int sock) {
+    while (true) {
+        string message;
+        cout << "Enter message: ";
+        getline(cin, message);
+        
+        if (message.empty()) continue;
+        
+        send(sock, message.c_str(), message.length(), 0);
+        cout << "Message sent" << endl;
+    }
+}
+
+int main(int argc, char *argv[]) {
     if (argc > 1) {
         PORT = atoi(argv[1]);
     }
 
-    int sock, new_socket;
-    long valread;
+    int sock;
     struct sockaddr_in serv_address;
-    int addrlen = sizeof(serv_address);
-    char *hello = "Hello from server";
 
-    char buffer[1024] = {0};
-
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-        perror("Socket Failed");
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0) {
+        perror("Socket creation failed");
         exit(EXIT_FAILURE);
     }
 
     serv_address.sin_family = AF_INET;
     serv_address.sin_port = htons(PORT);
 
-    if(inet_pton(AF_INET, "127.0.0.1", &serv_address.sin_addr) <= 0) {
+    if (inet_pton(AF_INET, "127.0.0.1", &serv_address.sin_addr) <= 0) {
         cout << "Invalid address" << endl;
+        close(sock);
         return -1;
     }
 
     if (connect(sock, (struct sockaddr *)&serv_address, sizeof(serv_address)) < 0) {
         cout << "Connection Failed" << endl;
+        close(sock);
         return -1;
     }
 
-    send(sock, hello, strlen(hello), 0);
-    cout << "Hello sent" << endl;
-    valread = read(sock, buffer, 1024);
-    cout << "Message Received: " << buffer << endl;
+    thread read_thread(read_messages, sock);
+    thread write_thread(write_messages, sock);
+
+    read_thread.join();
+    write_thread.join();
 
     return 0;
 }
--- myserver.cpp
+++ myserver.cpp
@@ -7,75 +7,86 @@
 #include <string>
 #include <thread>
 
-#define SOCKET_OPENING_ERROR -1
+#define SOCKET_CREATION_ERROR -1
 #define BIND_ERROR -2
-#define LISTEN_ERROR -2
-#define ZBS 0
+#define LISTEN_ERROR -3
+#define EXIT_SUCCESS 0
 
 using namespace std;
 const int PORT = 8888;
+const int BUFFER_SIZE = 1024;
 
-int main()
-{
-    int server_fd;
-    int new_socket;
-    long valread;
+void handle_client(int client_socket) {
+    while (true) {
+        
+        char buffer[BUFFER_SIZE] = {0};
+        long valread = read(client_socket, buffer, BUFFER_SIZE);
+        if (valread == 0) {
+            cout << "Client disconnected" << endl;
+            close(client_socket);
+            break;
+        } else if (valread < 0) {
+            cout << "Read error" << endl;
+            close(client_socket);
+            break;
+        }
+        cout << "Message received: " << buffer << endl;
 
+        
+        string response;
+        cout << "Enter response: ";
+        getline(cin, response);
+        if (response.empty()) {
+            cout << "No response sent." << endl;
+            continue;
+        }
+        send(client_socket, response.c_str(), response.length(), 0);
+        cout << "Response sent." << endl;
+    }
+}
+
+int main() {
+    int server_fd, client_socket;
     struct sockaddr_in address;
     int addrlen = sizeof(address);
 
-    cout << "Enter message: ";
-    string message;
-    cin >> message;
-
     server_fd = socket(AF_INET, SOCK_STREAM, 0);
-
-    if (server_fd == 0)
-    {
-        cout << "Socket failed" << endl;
-        exit(SOCKET_OPENING_ERROR);
+    if (server_fd < 0) {
+        cout << "Socket creation failed" << endl;
+        exit(SOCKET_CREATION_ERROR);
     }
 
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(PORT);
 
-    cout << PORT << endl;
-    cout << htons(PORT) << endl;
-
-    int bind_result = bind(server_fd, (struct sockaddr *)&address, sizeof(address));
-    if (bind_result < 0)
-    {
-        cout << "BIND Failed" << endl;
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        cout << "Bind failed" << endl;
         exit(BIND_ERROR);
     }
+    int yes = 1;
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes)) == -1) {
+        perror("setsockopt");
+        exit(1);
+    }
 
-    int listen_result = listen(server_fd, 10);
-    if (listen_result < 0)
-    {
-        cout << "Listen Failed" << endl;
+    if (listen(server_fd, 10) < 0) {
+        cout << "Listen failed" << endl;
         exit(LISTEN_ERROR);
     }
     cout << "Listening on port: " << PORT << endl;
 
-    while (true)
-    {
+    while (true) {
         cout << "Waiting for new connection ..." << endl;
-        new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen);
-        if (new_socket < 0)
-        {
+        client_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen);
+        if (client_socket < 0) {
+            cout << "Accept error" << endl;
             continue;
         }
-        char buffer[30000] = {0};
-        valread = read(new_socket, buffer, 30000);
-        cout << "Number of bytes received: " << valread << endl;
-        if (valread > 0)
-        {
-            cout << "Message received: " << buffer << endl;
-        }
-        write(new_socket, message.c_str(), message.length());
-        cout << "Hello message sent" << endl;
-        close(new_socket);
+        cout << "New connection, socket fd is " << client_socket << endl;
+
+        thread t(handle_client, client_socket);
+        t.detach();
     }
-    exit(ZBS);
+    return EXIT_SUCCESS;
 }
